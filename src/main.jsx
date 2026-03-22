a1cf4cee2f10af8141f9aee54501626184eb9c83
Vishal Kumar <vishi192222@gmail.com>
2026-03-22 21:03:50 +0530

    revert: remove window.__reseedStudents, use Dashboard button instead

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { reseedStudents } from './services/seedFirestore';
-window.__reseedStudents = reseedStudents;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
